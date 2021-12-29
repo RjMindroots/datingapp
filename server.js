c8a265a62cfdeaf24a5e535d8ca931d001e49ba2
user17 <rajpreetgiri4@gmail.com>
2021-12-29 18:11:21 +0530

    db connections

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 import express from "express";
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+const env = dotenv.config()
 const app = express();
 
+// mongodb connection
+mongoose.connect(`mongodb+srv://admin:mrtech@cluster0.svdho.mongodb.net/datingapp?retryWrites=true&w=majority`, 
+	{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+	}
+).then(() => {
+	console.log('Mongodb connected!')
+}).catch(err => console.log(err));
 
-app.listen(3000, () => {
-    console.log("app running")
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`app running on ${PORT}`)
 })
 
