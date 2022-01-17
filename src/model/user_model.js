d1326e34ecd072f71e62e6431e65d55c9c07dcb3
user17 <rajpreetgiri4@gmail.com>
2022-01-17 15:50:11 +0530

    adding new module for profile

--- src/model/user_model.js
+++ src/model/user_model.js
@@ -1,3 +1,4 @@
+import { string } from 'joi';
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
@@ -5,6 +6,9 @@ const userSchema = new Schema({
     user_name: { type: String, required: true },
     mobile: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    gender:{type:String},
+    city:{type:String},
+    interestIn:{type:String},
     role: { type: String, default: 'user' },
 }, { timestamps: true });
 
